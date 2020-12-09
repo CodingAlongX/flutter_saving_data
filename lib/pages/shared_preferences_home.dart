aefc3579d85d6d104434634966edf41ed463ab08
S. M. JAHANGIR <smjxpro@gmail.com>
2020-12-09 21:35:38 +0600

    added shared preferences

--- lib/pages/shared_preferences_home.dart
+++ lib/pages/shared_preferences_home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPreferencesHome extends StatelessWidget {
   @override
@@ -8,9 +9,43 @@ class SharedPreferencesHome extends StatelessWidget {
       appBar: AppBar(
         title: Text('Shared Preferences Home'),
       ),
-      body: Center(
-        child: Text('Shared Preferences Home'),
+      body: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              child: Text('Read'),
+              onPressed: () {
+                _read();
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              child: Text('Save'),
+              onPressed: () {
+                _save();
+              },
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  _read() async {
+    final prefs = await SharedPreferences.getInstance();
+    final key = 'my_int_key';
+    final value = prefs.getInt(key) ?? 0;
+    print('Read: $value');
+  }
+
+  _save() async {
+    final prefs = await SharedPreferences.getInstance();
+    final key = 'my_int_key';
+    final value = 42;
+    prefs.setInt(key, value);
+    print('Saved: $value');
   }
 }
