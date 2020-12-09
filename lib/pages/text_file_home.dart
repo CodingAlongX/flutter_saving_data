0e3a661fe736737ed595c1450674c09bcb305a99
S. M. JAHANGIR <smjxpro@gmail.com>
2020-12-09 22:28:53 +0600

    added saving to text file

--- lib/pages/text_file_home.dart
+++ lib/pages/text_file_home.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 class TextFileHome extends StatelessWidget {
   @override
@@ -8,9 +11,47 @@ class TextFileHome extends StatelessWidget {
       appBar: AppBar(
         title: Text('Text File Home'),
       ),
-      body: Center(
-        child: Text('Text File Home'),
+      body: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              child: Text('Read'),
+              onPressed: () {
+                _read();
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              child: Text('Save'),
+              onPressed: () {
+                _save();
+              },
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  _read() async {
+    try {
+      final directory = await getApplicationDocumentsDirectory();
+      final file = File('${directory.path}/my_file.txt');
+      String text = await file.readAsString();
+      print(text);
+    } catch (e) {
+      print("Couldn't read file");
+    }
+  }
+
+  _save() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final file = File('${directory.path}/my_file.txt');
+    final text = 'Hello World!';
+    await file.writeAsString(text);
+    print('Saved');
   }
 }
